829f4a12ee1ec425528bc0283e37ce9a5f59a64b
quoctrongdev-prog <quoctrongdev@gmail.com>
2026-04-19 17:46:31 +0700

    Delete user/dist directory

--- user/dist/utils/buffer.js
+++
@@ -1,8 +0,0 @@
-import DataUriParser from 'datauri/parser.js';
-import path from 'path';
-const getBuffer = (file) => {
-    const parser = new DataUriParser();
-    const extname = path.extname(file.originalname).toString();
-    return parser.format(extname, file.buffer);
-};
-export default getBuffer;
